0155e7bad4c7c4d8024a624eaccc01be41742062
priyanka-kumari <priyanka.kumari@npstx.com>
2024-08-12 12:06:37 +0530

    Service name changed and db.migration removed.

--- src/main/java/com/swiggy/app/demo/OrderServiceApplication.java
+++ src/main/java/com/swiggy/app/demo/OrderServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplicationApplication {
+public class OrderServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplicationApplication.class, args);
+		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
 }
